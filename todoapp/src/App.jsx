2a15a0af28c04a2d6d2f1b307df76ba6ae28f6e6
tomi1130 <tomiyama891@gmail.com>
2021-09-13 23:43:49 +0900

    section8 finish

---
+++ todoapp/src/App.jsx
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from "axios";
+
+function App() {
+  const onClickUsers=()=>{
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res)=>{
+      console.log(res.data);
+    }).catch((err)=>console.log(err));
+  }
+  const onClickUser1=()=>{
+    axios.get("https://jsonplaceholder.typicode.com/users/3").then((res)=>{
+      console.log(res.data);
+    }).catch((err)=>console.log(err));
+  }
+  return (
+    <div className="App">
+     <button onClick={onClickUsers}>users</button>
+     <button onClick={onClickUser1}>id=1のユーザー</button>
+    </div>
+  );
+}
+
+export default App;
